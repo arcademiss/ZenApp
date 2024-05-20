a917c7f37b67acfe0f6775f8d98a8273902d1ea9
arcademiss <georgeventel97@gmail.com>
2024-05-20 22:29:26 +0300

    Creare functii pentru song player Creare functie pentru give up

--- ZenAppClient/ZenAppClient/Form1.Designer.cs
+++ ZenAppClient/ZenAppClient/Form1.Designer.cs
@@ -2,8 +2,6 @@
 {
     partial class Form1
     {
-
-
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -30,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonPlaySong = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPlaySong
+            // 
+            this.buttonPlaySong.Location = new System.Drawing.Point(50, 254);
+            this.buttonPlaySong.Name = "buttonPlaySong";
+            this.buttonPlaySong.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlaySong.TabIndex = 0;
+            this.buttonPlaySong.Text = "button1";
+            this.buttonPlaySong.UseVisualStyleBackColor = true;
+            this.buttonPlaySong.Click += new System.EventHandler(this.buttonPlaySong_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonGiveUp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonPlaySong);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonPlaySong;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ZenAppClient/ZenAppClient/Form1.cs
+++ ZenAppClient/ZenAppClient/Form1.cs
@@ -7,14 +7,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Media;
+using NAudio.Wave;
 namespace ZenAppClient
 {
     public partial class Form1 : Form
     {
+        ZenAppClient.ServiceReference1.WebService1SoapClient service = new ServiceReference1.WebService1SoapClient();
         public Form1()
         {
             InitializeComponent();
+
+
         }
+
+
+        private WaveOutEvent waveOut;
+        private AudioFileReader audioFileReader;
+        private String existingPath=null;
+        private int ZenPoints = 1000;
+        private int roundNumber = 1;
+
+        private void buttonPlaySong_Click(object sender, EventArgs e)
+        {  
+           /// This function is called when the play song button is clicked. What happens is that the function retrieves
+           /// a random song if one is not already selected and it is played.
+           
+            
+            //Stop any song already playing
+
+            waveOut?.Stop();
+            waveOut?.Dispose();
+            audioFileReader?.Dispose();
+
+            String path = get_Song(); // here we will use the service to retrieve a song
+
+            playSong(path, waveOut, audioFileReader);
+        }
+
+        private String get_Song()
+        {
+
+            /// Checks if the global path is null or not. If it is then it means that the user is on the first round
+            /// If it is not null then it means the user is playing the same round and we just replay the song.
+            String path2Song = "";
+            if(existingPath != null)
+                path2Song = existingPath;
+            else
+            {
+                //TODO: path2Song=service.GetRandomSong
+                path2Song = "C:\\an3\\s2\\II\\dwnl\\Creep [Explicit].mp4";
+                existingPath = path2Song;
+            }
+
+
+            
+
+            return path2Song;
+        }
+
+        private void playSong(String path, WaveOutEvent waveOut, AudioFileReader audioFileReader)
+        {
+            /// sets up the song player, plays the song and ticks a counter for 10 seconds
+            try
+            {
+                //Init file reader and wave output
+                audioFileReader = new AudioFileReader(path);
+                waveOut = new WaveOutEvent();
+
+                //play the audio file
+                waveOut.Init(audioFileReader);
+                waveOut.Play();
+
+                // set a timer for 10 seconds
+
+                Timer timer = new Timer();
+
+                timer.Interval = 10000;
+                timer.Tick += (s, args) =>
+                {
+                    waveOut.Stop();
+                    timer.Stop();
+                };
+                timer.Start();
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Error playing audio file" + ex.Message);
+            }
+
+        }
+
+
+        private void buttonGiveUp_Click(object sender, EventArgs e)
+        {
+            //1.Display message box, passive agressive
+            //TODO: get random passive agressive text.
+
+            MessageBox.Show("hAH KYS");
+
+
+            //2.Decrease zen points
+            ZenPoints -= 200;
+
+            //3. Increase the round count
+            roundNumber++;
+            //4a. If it is greater than 3 then ask the user for their name and finish the game.
+            if(roundNumber > 3)
+            {
+                //TODO:Create form asking for name
+                //TODO: add name and points to database
+                //when the data is sent to the database and the form is closed the game is also closed
+            }
+            //4b.Pick a new song(set the global path to null).
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            waveOut?.Stop();
+            waveOut?.Dispose();
+            audioFileReader?.Dispose();
+            base.OnFormClosing(e);
+        }
+        
     }
 }
